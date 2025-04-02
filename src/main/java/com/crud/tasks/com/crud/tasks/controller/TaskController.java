6903cfba3a40452af03070970991f73fe6654fe3
Piotr-Trzcinski <cinekenic@go2.pl>
2025-04-02 17:05:23 +0200

    17.4 mapping

---
+++ src/main/java/com/crud/tasks/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,38 @@
+package com.crud.tasks.com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/tasks")
+public class TaskController {
+
+//    @RequestMapping(method = RequestMethod.GET, value = "")
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+//    @RequestMapping(method = RequestMethod.GET, value = "/{taskId}")
+    @GetMapping("/{taskId}")
+    public TaskDto getTask(Long taskId) {
+        return new TaskDto(1L, "test title", "test_content");
+    }
+
+//    @RequestMapping(method = RequestMethod.DELETE, value = "/{taskId}")
+    @DeleteMapping("/{taskId}")
+    public void deleteTask(Long taskId) {}
+
+//    @RequestMapping(method = RequestMethod.PUT, value = "")
+    @PutMapping
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto(1L, "Edited test title", "Test content");
+    }
+
+//    @RequestMapping(method = RequestMethod.POST, value = "")
+    @PostMapping
+    public void createTask(TaskDto taskDto) {}
+}
